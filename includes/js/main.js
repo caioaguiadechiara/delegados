8fddf63de37350c0f7f233b269af037ec8e39304
Caio Águia <caguia@elsauce.cl>
2025-03-20 19:13:07 -0300

    prevent inspeccionar

--- includes/js/main.js
+++ includes/js/main.js
@@ -1,3 +1,13 @@
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+document.addEventListener('keydown', (e) => {
+  if (e.key === "F12" || 
+      (e.ctrlKey && e.shiftKey && e.key === "I") || 
+      (e.ctrlKey && e.shiftKey && e.key === "C") || 
+      (e.ctrlKey && e.key === "U")) {
+      e.preventDefault();
+  }
+});
+
 function verPassword() {
   var estadoVerPass = $('#inputPass').attr('type');
   if (estadoVerPass == 'password') {
